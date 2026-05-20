ec07fb54554fd1245904eca09718aae82d1364c8
hisudewaltz <24022329@vnu.edu.vn>
2026-05-20 21:30:24 +0700

    au

--- src/backend/tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from .celery_app import celery_app
-except Exception:  # pragma: no cover - lets the API boot even before celery is installed
-    celery_app = None
-
-from .video_tasks import enqueue_video_processing, process_video_task
-
-__all__ = ["celery_app", "enqueue_video_processing", "process_video_task"]
